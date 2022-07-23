f98b2b7bb87e9dbe1463b3d7912fe55fc3bef77c
edyta2770 <edyta.elert27@gmail.com>
2022-07-23 13:29:05 +0200

    tablica kotkow

---
+++ src/ForLoop.kt
@@ -0,0 +1,10 @@
+class ForLoop {
+    static void main (String[] args)
+
+    String [] catBoard = new String[] {"karmi", "Bubu", "Simba", "Tuflik", "Parker"};
+    for (int i = 0; i < catBoard.lenght; i++) {
+        System.out.printIn(catBoard[i]);
+    }
+    for (string cat: catBoard) {
+        System.out.printIn(cat);
+    }
--- src/TextFromUser.kt
+++ src/TextFromUser.kt
@@ -3,6 +3,6 @@ import java.util.Scanner
 class TextFromUser {
     static void main (String[] args) {
     Scanner scanner = new Scanner (System.in);
+        System.out.printIn("Podaj nazwe uzytkownika");
         String userInput = scanner.nextline();
-        System.out.printIn("witaj uzytkownik"+userInput);
 }
