18b6683db059870b555987b450bc37226cce4a4a
edyta2770 <edyta.elert27@gmail.com>
2022-07-23 13:37:13 +0200

    tablica kotkow

---
+++ src/ForLoop.java
@@ -0,0 +1,14 @@
+class ForLoop {
+    static void main(String[] args) {
+
+        String[] catBoard = new String[]{"karmi", "Bubu", "Simba", "Tuflik", "Parker"};
+        for (int i = 0; i < catBoard.length; i++) {
+            System.out.println(catBoard[i]);
+        }
+        for (String cat : catBoard) {
+            System.out.println(cat);
+        }
+    }
+}
+
+
